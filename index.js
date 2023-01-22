7864e439207c713bad522e6bfe0724563da98db3
M. H. Nahib <mhnahib@gmail.com>
2023-01-22 23:39:17 +0600

    removed old process added making new file process

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
+const nodeIgnore = require("./node-ignore");
 
-fs.copyFile("./node-ignore.txt", ".gitignore", (err) => {
+// fs.copyFile("./node-ignore.txt", ".gitignore", (err) => {
+//   if (err) throw err;
+//   console.log("Successfully created .gitignore for node");
+// });
+
+fs.writeFile(".gitignore", nodeIgnore, function (err) {
   if (err) throw err;
   console.log("Successfully created .gitignore for node");
 });
